4dba640a9257fa542941667315e9a6c52806ac68
Chinmay Purav <chinpurav@gmail.com>
2024-05-11 00:29:22 +0530

    ExpenseTest.php added

---
+++ tests/Feature/Models/ExpenseTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\Account;
+use App\Models\Expense;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('belongs to a user', function () {
+    $expense = Expense::factory()->has(User::factory())->create();
+
+    expect($expense->user)->toBeInstanceOf(User::class);
+});
+
+it('belongs to a account', function () {
+    $expense = Expense::factory()->has(Account::factory())->create();
+
+    expect($expense->account)->toBeInstanceOf(Account::class);
+});
