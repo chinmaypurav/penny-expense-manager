050d79bb15e66501960e0a7085904d9c2b4aa05e
Chinmay Purav <chinpurav@gmail.com>
2024-06-09 13:11:34 +0530

    Frequency.php enum added

---
+++ app/Enums/Frequency.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+enum Frequency: string
+{
+    case DAILY = 'daily';
+    case WEEKLY = 'weekly';
+    case MONTHLY = 'monthly';
+    case QUARTERLY = 'quarterly';
+    case YEARLY = 'yearly';
+
+    public static function all(): array
+    {
+        return array_column(self::cases(), 'value', 'value');
+    }
+}
