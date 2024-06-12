d520a37393d0973130975a84ccea812462d0dedb
Chinmay Purav <chinpurav@gmail.com>
2024-06-12 21:02:43 +0530

    TransactRecurringIncomeJob.php added

--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,24 @@
 <?php
 
+use App\Enums\Frequency;
 use App\Enums\RecordType;
 use App\Jobs\CreatePeriodicalBalanceEntryJob;
+use App\Jobs\TransactRecurringIncomeJob;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(new CreatePeriodicalBalanceEntryJob(RecordType::MONTHLY, today()))
     ->monthly();
 
 Schedule::job(new CreatePeriodicalBalanceEntryJob(RecordType::YEARLY, today()))
+    ->yearly();
+
+Schedule::job(new TransactRecurringIncomeJob(Frequency::DAILY))
+    ->daily();
+Schedule::job(new TransactRecurringIncomeJob(Frequency::WEEKLY))
+    ->weekly();
+Schedule::job(new TransactRecurringIncomeJob(Frequency::MONTHLY))
+    ->monthly();
+Schedule::job(new TransactRecurringIncomeJob(Frequency::QUARTERLY))
+    ->quarterly();
+Schedule::job(new TransactRecurringIncomeJob(Frequency::YEARLY))
     ->yearly();
