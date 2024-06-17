06c85ad01e13c247d7f1923dc68cf3f531f87ba1
Chinmay Purav <chinpurav@gmail.com>
2024-06-18 00:34:16 +0530

    Factory fix

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -15,7 +15,7 @@ class CategoryFactory extends Factory
         return [
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
-            'name' => fake()->name(),
+            'name' => fake()->word(),
         ];
     }
 }
