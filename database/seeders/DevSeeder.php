8740c25a7100da1637cd95bec0eb74729a19f30b
Chinmay Purav <chinpurav@gmail.com>
2024-06-18 00:34:25 +0530

    DevSeeder.php fix

--- database/seeders/DevSeeder.php
+++ database/seeders/DevSeeder.php
@@ -3,10 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Account;
+use App\Models\Category;
 use App\Models\Expense;
 use App\Models\Income;
+use App\Models\Person;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DevSeeder extends Seeder
@@ -15,17 +16,19 @@ class DevSeeder extends Seeder
     {
         $user = $this->user();
         $accounts = Account::factory()->for($user)->count(5)->create();
+        $categories = Category::factory()->count(10)->create();
+        $people = Person::factory()->count(15)->create();
 
         Income::factory()->for($user)->count(100)
-            ->state(new Sequence(
-                fn (Sequence $sequence) => ['account_id' => $accounts->random()],
-            ))
+            ->recycle($accounts)
+            ->recycle($categories)
+            ->recycle($people)
             ->create();
 
         Expense::factory()->for($user)->count(100)
-            ->state(new Sequence(
-                fn (Sequence $sequence) => ['account_id' => $accounts->random()],
-            ))
+            ->recycle($accounts)
+            ->recycle($categories)
+            ->recycle($people)
             ->create();
     }
 
