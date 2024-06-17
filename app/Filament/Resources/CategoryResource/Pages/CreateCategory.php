03fbce38bc55078008670b1f3c26c422da1078e3
Chinmay Purav <chinpurav@gmail.com>
2024-06-17 21:01:03 +0530

    category model added

---
+++ app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\CategoryResource\Pages;
+
+use App\Filament\Resources\CategoryResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCategory extends CreateRecord
+{
+    protected static string $resource = CategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+
+        ];
+    }
+}
