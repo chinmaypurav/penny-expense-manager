7a0db58c644cedb4574bd15b215d8caeb0784f83
Chinmay Purav <chinpurav@gmail.com>
2024-06-17 21:01:03 +0530

    category model added

---
+++ app/Filament/Resources/CategoryResource/Pages/EditCategory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\CategoryResource\Pages;
+
+use App\Filament\Resources\CategoryResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\RestoreAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCategory extends EditRecord
+{
+    protected static string $resource = CategoryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+            ForceDeleteAction::make(),
+            RestoreAction::make(),
+        ];
+    }
+}
