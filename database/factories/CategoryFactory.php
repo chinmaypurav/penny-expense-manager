5de5eb9379f3562d1a97dd405df8be7c4c2ec596
Chinmay Purav <chinpurav@gmail.com>
2024-06-17 21:01:03 +0530

    category model added

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class CategoryFactory extends Factory
+{
+    protected $model = Category::class;
+
+    public function definition(): array
+    {
+        return [
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'name' => fake()->name(),
+        ];
+    }
+}
