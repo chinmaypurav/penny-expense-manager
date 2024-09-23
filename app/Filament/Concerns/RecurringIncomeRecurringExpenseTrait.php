a8410fcddca1150e9b720acf5b0527ce8fabcac2
chinmaypurav <chinpurav@gmail.com>
2024-09-23 22:52:01 +0530

    RecurringIncomeRecurringExpenseTrait fixed

--- app/Filament/Concerns/RecurringIncomeRecurringExpenseTrait.php
+++ app/Filament/Concerns/RecurringIncomeRecurringExpenseTrait.php
@@ -70,11 +70,11 @@ trait RecurringIncomeRecurringExpenseTrait
 
                 Placeholder::make('created_at')
                     ->label('Created Date')
-                    ->content(fn (?RecurringIncome $record): string => $record?->created_at?->diffForHumans() ?? '-'),
+                    ->content(fn (RecurringIncome|RecurringExpense|null $record): string => $record?->created_at?->diffForHumans() ?? '-'),
 
                 Placeholder::make('updated_at')
                     ->label('Last Modified Date')
-                    ->content(fn (?RecurringIncome $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
+                    ->content(fn (RecurringIncome|RecurringExpense|null $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
             ]);
     }
 
