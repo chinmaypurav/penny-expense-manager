74c08b88960a649a27ddd301ffc5e87007cad07a
chinmaypurav <chinpurav@gmail.com>
2024-09-23 22:55:03 +0530

    TransactRecurringExpenseJob.php added with test

---
+++ app/Jobs/TransactRecurringExpenseJob.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Enums\Frequency;
+use App\Models\Expense;
+use App\Models\RecurringExpense;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class TransactRecurringExpenseJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(private readonly Frequency $frequency) {}
+
+    public function handle(): void
+    {
+        DB::transaction(function () {
+            $fillable = (new Expense)->getFillable();
+
+            RecurringExpense::query()
+                ->where('frequency', $this->frequency)
+                ->get()
+                ->map(function (RecurringExpense $recurringExpense) use ($fillable) {
+                    $data = $recurringExpense->only($fillable);
+
+                    $data['transacted_at'] = $recurringExpense->next_transaction_at;
+                    $expense = Expense::create($data);
+
+                    $expense->tags()->attach($recurringExpense->tags);
+
+                    $this->processRecurringExpenseState($recurringExpense);
+                });
+        });
+    }
+
+    private function processRecurringExpenseState(RecurringExpense $recurringExpense): void
+    {
+        if ($recurringExpense->remaining_recurrences === null) {
+            return;
+        }
+
+        if ($recurringExpense->remaining_recurrences === 1) {
+            $recurringExpense->delete();
+
+            return;
+        }
+
+        $recurringExpense->decrement('remaining_recurrences');
+    }
+}
---
+++ tests/Feature/Jobs/TransactRecurringExpenseJobTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Jobs;
+
+use App\Jobs\TransactRecurringExpenseJob;
+use App\Models\Expense;
+use App\Models\RecurringExpense;
+use App\Models\Tag;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\assertDatabaseMissing;
+use function PHPUnit\Framework\assertEquals;
+
+uses(DatabaseMigrations::class);
+
+test('it copies recurring expense into expense', function () {
+    $recurringExpense = RecurringExpense::factory()->create();
+
+    (new TransactRecurringExpenseJob($recurringExpense->frequency))->handle();
+
+    assertDatabaseHas(Expense::class, [
+        'description' => $recurringExpense->description,
+        'amount' => $recurringExpense->amount,
+        'account_id' => $recurringExpense->account_id,
+        'person_id' => $recurringExpense->person_id,
+        'category_id' => $recurringExpense->category_id,
+        'user_id' => $recurringExpense->user_id,
+        'transacted_at' => $recurringExpense->next_transaction_at,
+    ]);
+});
+
+test('it copies recurring expense tags to expense', function () {
+    $tags = Tag::factory()->count(3)->create();
+
+    $recurringExpense = RecurringExpense::factory()->create();
+    $recurringExpense->tags()->attach($tags);
+
+    (new TransactRecurringExpenseJob($recurringExpense->frequency))->handle();
+
+    $expenseTags = Expense::first()->tags->pluck('id')->toArray();
+
+    assertEquals($tags->pluck('id')->toArray(), $expenseTags);
+
+});
+
+test('it decrements remaining_recurrences by 1', function () {
+    $recurringExpense = RecurringExpense::factory()->create([
+        'remaining_recurrences' => fake()->numberBetween(2, 10),
+    ]);
+
+    $job = new TransactRecurringExpenseJob($recurringExpense->frequency);
+
+    $job->handle();
+
+    assertDatabaseHas(RecurringExpense::class, [
+        'id' => $recurringExpense->id,
+        'remaining_recurrences' => $recurringExpense->remaining_recurrences - 1,
+    ]);
+
+});
+
+test('it keeps remaining_recurrences null when null remaining_recurrences', function () {
+    $recurringExpense = RecurringExpense::factory()->create([
+        'remaining_recurrences' => null,
+    ]);
+
+    $job = new TransactRecurringExpenseJob($recurringExpense->frequency);
+
+    $job->handle();
+
+    assertDatabaseHas(RecurringExpense::class, [
+        'id' => $recurringExpense->id,
+        'remaining_recurrences' => null,
+    ]);
+
+});
+
+test('it deletes remaining_recurrences remaining_recurrences is 1', function () {
+    $recurringExpense = RecurringExpense::factory()->create([
+        'remaining_recurrences' => 1,
+    ]);
+
+    $job = new TransactRecurringExpenseJob($recurringExpense->frequency);
+
+    $job->handle();
+
+    assertDatabaseMissing(RecurringExpense::class, [
+        'id' => $recurringExpense->id,
+    ]);
+
+});
