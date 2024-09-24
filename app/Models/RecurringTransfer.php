8b76cf82b115f630832f424329c07ff58dad6aa0
chinmaypurav <chinpurav@gmail.com>
2024-09-24 22:06:58 +0530

    model fillable, casts and relationships

--- app/Models/RecurringTransfer.php
+++ app/Models/RecurringTransfer.php
@@ -2,10 +2,52 @@
 
 namespace App\Models;
 
+use App\Enums\Frequency;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class RecurringTransfer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'creditor_id',
+        'debtor_id',
+        'description',
+        'amount',
+        'next_transaction_at',
+        'frequency',
+        'remaining_recurrences',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'next_transaction_at' => 'date:Y-m-d',
+            'frequency' => Frequency::class,
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function creditor(): BelongsTo
+    {
+        return $this->belongsTo(Account::class, 'creditor_id');
+    }
+
+    public function debtor(): BelongsTo
+    {
+        return $this->belongsTo(Account::class, 'debtor_id');
+    }
+
+    public function tags(): MorphToMany
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
 }
