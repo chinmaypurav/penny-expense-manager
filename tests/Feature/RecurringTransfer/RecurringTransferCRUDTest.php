4a3ae2d118e56f2841674e6337862b865a75dd64
chinmaypurav <chinpurav@gmail.com>
2024-09-24 22:13:32 +0530

    RecurringTransferCRUDTest.php added

---
+++ tests/Feature/RecurringTransfer/RecurringTransferCRUDTest.php
@@ -0,0 +1,124 @@
+<?php
+
+use App\Filament\Resources\RecurringTransferResource;
+use App\Models\Account;
+use App\Models\RecurringTransfer;
+use App\Models\User;
+use Filament\Actions\DeleteAction;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
+
+use function Pest\Livewire\livewire;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+    $this->actingAs($this->user);
+    Carbon::setTestNow(now());
+});
+
+it('can render recurring recurring transfers list page', function () {
+    RecurringTransfer::factory()->for($this->user)->create();
+
+    $this->get(RecurringTransferResource::getUrl('index'))->assertSuccessful();
+});
+
+it('can create recurring transfer', function () {
+
+    $newData = RecurringTransfer::factory()->make();
+    $creditor = Account::factory()->create();
+    $debtor = Account::factory()->create();
+
+    livewire(RecurringTransferResource\Pages\CreateRecurringTransfer::class)
+        ->fillForm([
+            'description' => $newData->description,
+            'creditor_id' => $creditor->id,
+            'debtor_id' => $debtor->id,
+            'amount' => $newData->amount,
+            'next_transaction_at' => $newData->next_transaction_at,
+            'remaining_recurrences' => $newData->remaining_recurrences,
+            'frequency' => $newData->frequency,
+        ])
+        ->call('create')
+        ->assertHasNoFormErrors();
+
+    $this->assertDatabaseHas(RecurringTransfer::class, [
+        'description' => $newData->description,
+        'creditor_id' => $creditor->id,
+        'debtor_id' => $debtor->id,
+        'amount' => $newData->amount,
+        'next_transaction_at' => $newData->next_transaction_at->toDateString(),
+        'remaining_recurrences' => $newData->remaining_recurrences,
+        'frequency' => $newData->frequency,
+    ]);
+});
+
+it('can render recurring transfer edit page', function () {
+    $this->get(RecurringTransferResource::getUrl('edit', [
+        'record' => RecurringTransfer::factory()->for($this->user)->create(),
+    ]))->assertSuccessful();
+});
+
+it('can retrieve recurring transfer data', function () {
+    $recurringTransfer = RecurringTransfer::factory()->for($this->user)->create();
+
+    livewire(RecurringTransferResource\Pages\EditRecurringTransfer::class, [
+        'record' => $recurringTransfer->getRouteKey(),
+    ])
+        ->assertFormSet([
+            'description' => $recurringTransfer->description,
+            'creditor_id' => $recurringTransfer->creditor_id,
+            'debtor_id' => $recurringTransfer->debtor_id,
+            'amount' => $recurringTransfer->amount,
+            'next_transaction_at' => $recurringTransfer->next_transaction_at->toDateString(),
+            'remaining_recurrences' => $recurringTransfer->remaining_recurrences,
+            'frequency' => $recurringTransfer->frequency->value,
+        ]);
+});
+
+it('can update recurring transfer', function () {
+
+    $recurringTransfer = RecurringTransfer::factory()->for($this->user)->create();
+
+    $creditor = Account::factory()->create();
+    $debtor = Account::factory()->create();
+    $newData = RecurringTransfer::factory()->make();
+
+    livewire(RecurringTransferResource\Pages\EditRecurringTransfer::class, [
+        'record' => $recurringTransfer->getRouteKey(),
+    ])
+        ->fillForm([
+            'description' => $newData->description,
+            'creditor_id' => $creditor->id,
+            'debtor_id' => $debtor->id,
+            'amount' => $newData->amount,
+            'next_transaction_at' => $newData->next_transaction_at,
+            'remaining_recurrences' => $newData->remaining_recurrences,
+            'frequency' => $newData->frequency,
+        ])
+        ->call('save')
+        ->assertHasNoFormErrors();
+
+    $this->assertDatabaseHas(RecurringTransfer::class, [
+        'description' => $newData->description,
+        'creditor_id' => $creditor->id,
+        'debtor_id' => $debtor->id,
+        'amount' => $newData->amount,
+        'next_transaction_at' => $newData->next_transaction_at->toDateString(),
+        'remaining_recurrences' => $newData->remaining_recurrences,
+        'frequency' => $newData->frequency,
+    ]);
+});
+
+it('can delete recurring transfer', function () {
+
+    $recurringTransfer = RecurringTransfer::factory()->for($this->user)->create();
+
+    livewire(RecurringTransferResource\Pages\EditRecurringTransfer::class, [
+        'record' => $recurringTransfer->getRouteKey(),
+    ])
+        ->callAction(DeleteAction::class);
+
+    $this->assertModelMissing($recurringTransfer);
+});
