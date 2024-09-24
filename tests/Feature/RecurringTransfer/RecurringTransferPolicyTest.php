a9544188b52271c6a9018e7acd511f493cf90f6c
chinmaypurav <chinpurav@gmail.com>
2024-09-24 22:18:32 +0530

    RecurringTransferPolicyTest.php added

---
+++ tests/Feature/RecurringTransfer/RecurringTransferPolicyTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Filament\Resources\RecurringTransferResource;
+use App\Models\RecurringTransfer;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use function Pest\Livewire\livewire;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+    $this->actingAs($this->user);
+
+    RecurringTransfer::factory()->for($this->user)->create([
+        'description' => 'User 1 Recurring Transfer',
+    ]);
+
+    RecurringTransfer::factory()->for(User::factory())->create([
+        'description' => 'User 2 Recurring Transfer',
+    ]);
+});
+
+it('can render recurring transfers list page with only current user recurring transfers', function () {
+
+    $this->get(RecurringTransferResource::getUrl('index'))
+        ->assertSuccessful()
+        ->assertSee('User 1 Recurring Transfer')
+        ->assertDontSee('User 2 Recurring Transfer');
+});
+
+it('cannot render recurring transfer edit page for another user', function () {
+    $this->get(RecurringTransferResource::getUrl('edit', [
+        'record' => RecurringTransfer::factory()->create(),
+    ]))->assertForbidden();
+});
+
+it('cannot retrieve recurring transfer data for another user', function () {
+    $recurringTransfer = RecurringTransfer::factory()->create();
+
+    livewire(RecurringTransferResource\Pages\EditRecurringTransfer::class, [
+        'record' => $recurringTransfer->getRouteKey(),
+    ])
+        ->assertForbidden();
+});
