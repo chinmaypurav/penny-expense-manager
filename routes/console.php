7a376e593b7d86afff69d61e1731793fd6b1dc38
chinmaypurav <chinpurav@gmail.com>
2024-09-24 22:22:28 +0530

    jobs set to run via schedule

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use App\Enums\Frequency;
 use App\Enums\RecordType;
 use App\Jobs\CreatePeriodicalBalanceEntryJob;
+use App\Jobs\TransactRecurringExpenseJob;
 use App\Jobs\TransactRecurringIncomeJob;
 use Illuminate\Support\Facades\Schedule;
 
@@ -21,4 +22,15 @@ Schedule::job(new TransactRecurringIncomeJob(Frequency::MONTHLY))
 Schedule::job(new TransactRecurringIncomeJob(Frequency::QUARTERLY))
     ->quarterly();
 Schedule::job(new TransactRecurringIncomeJob(Frequency::YEARLY))
+    ->yearly();
+
+Schedule::job(new TransactRecurringExpenseJob(Frequency::DAILY))
+    ->daily();
+Schedule::job(new TransactRecurringExpenseJob(Frequency::WEEKLY))
+    ->weekly();
+Schedule::job(new TransactRecurringExpenseJob(Frequency::MONTHLY))
+    ->monthly();
+Schedule::job(new TransactRecurringExpenseJob(Frequency::QUARTERLY))
+    ->quarterly();
+Schedule::job(new TransactRecurringExpenseJob(Frequency::YEARLY))
     ->yearly();
