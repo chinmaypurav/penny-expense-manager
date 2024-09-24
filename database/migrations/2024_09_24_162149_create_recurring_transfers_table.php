fcf011e8d33d79ffb88af2b0ea4857ed851d1518
chinmaypurav <chinpurav@gmail.com>
2024-09-24 22:00:29 +0530

    migration model factory

---
+++ database/migrations/2024_09_24_162149_create_recurring_transfers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('recurring_transfers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('creditor_id');
+            $table->foreignId('debtor_id');
+            $table->string('description')->nullable();
+            $table->timestamp('transacted_at');
+            $table->decimal('amount', 65);
+            $table->date('next_transaction_at');
+            $table->string('frequency')->index();
+            $table->unsignedBigInteger('remaining_recurrences')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('recurring_transfers');
+    }
+};
