d1bbc95668e171582731c56ba1dca2736ffb0f3c
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2024-12-15 01:23:43 +0530

    feat: Add CSV export functionality for Tags, Categories, and People (#13)
    * Add export functionality to PersonResource
    
    Introduced a `PersonExporter` class to handle exporting Person data with defined columns and notifications. Updated `ListPeople` to include an export action supporting CSV format, enhancing data management capabilities.
    
    * Add export functionality for categories
    
    Implemented a `CategoryExporter` class to handle category exports with specific columns and notifications. Integrated an export action into the category list page, supporting CSV format. This enhancement improves data handling and accessibility for category records.
    
    * Add Tag export functionality with CSV support
    
    Introduced `TagExporter` to handle exporting tags with columns for ID, name, and color. Integrated an export action into the `ListTags` page, enabling CSV export and providing notification details for successes and failures.

--- app/Filament/Resources/TagResource/Pages/ListTags.php
+++ app/Filament/Resources/TagResource/Pages/ListTags.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\TagResource\Pages;
 
+use App\Filament\Exports\TagExporter;
 use App\Filament\Imports\TagImporter;
 use App\Filament\Resources\TagResource;
 use Filament\Actions\CreateAction;
+use Filament\Actions\ExportAction;
+use Filament\Actions\Exports\Enums\ExportFormat;
 use Filament\Actions\ImportAction;
 use Filament\Resources\Pages\ListRecords;
 
@@ -18,6 +21,11 @@ class ListTags extends ListRecords
             CreateAction::make(),
             ImportAction::make()
                 ->importer(TagImporter::class),
+            ExportAction::make()
+                ->exporter(TagExporter::class)
+                ->formats([
+                    ExportFormat::Csv,
+                ]),
         ];
     }
 }
