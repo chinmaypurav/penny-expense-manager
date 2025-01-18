a6803a69d5c1f54b489454fee35b687c88a1f067
Chinmay Purav <chinpurav@gmail.com>
2025-01-18 07:42:21 +0530

    cascade on delete

--- database/migrations/2024_05_31_194116_create_balances_table.php
+++ database/migrations/2024_05_31_194116_create_balances_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('balances', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Account::class)->constrained();
+            $table->foreignIdFor(Account::class)->constrained()->cascadeOnDelete();
             $table->decimal('balance', 65);
             $table->date('recorded_until');
             $table->boolean('is_initial_record')->default(false);
