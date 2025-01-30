0572c39d13d33e1abb8f092a9bfec2ae7400b509
Chinmay Purav <chinpurav@gmail.com>
2025-01-31 04:03:51 +0530

    methods added to enum

--- app/Enums/PanelId.php
+++ app/Enums/PanelId.php
@@ -6,4 +6,44 @@ enum PanelId: string
 {
     case APP = 'app';
     case FAMILY = 'family';
+
+    public function getId(): string
+    {
+        return match ($this) {
+            self::APP => 'app',
+            self::FAMILY => 'family',
+        };
+    }
+
+    public function getPath(): string
+    {
+        return match ($this) {
+            self::APP => 'app',
+            self::FAMILY => 'family',
+        };
+    }
+
+    public function getHomeUrl(): string
+    {
+        return match ($this) {
+            self::APP => 'app',
+            self::FAMILY => 'family',
+        };
+    }
+
+    public function getSwitchButtonLabel(): string
+    {
+        return match ($this) {
+            self::APP => 'Individual view',
+            self::FAMILY => 'Family view',
+        };
+    }
+
+    public function getSwitchButtonIcon(): string
+    {
+        return match ($this) {
+            self::APP => 'heroicon-o-user-circle',
+            self::FAMILY => 'heroicon-o-user-group',
+        };
+    }
 }
