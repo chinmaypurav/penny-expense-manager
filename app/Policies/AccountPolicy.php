cf24a7c3c1af923420ceedce104e41122a57b032
Chinmay Purav <chinpurav@gmail.com>
2025-02-01 17:02:29 +0530

    account policy changes

--- app/Policies/AccountPolicy.php
+++ app/Policies/AccountPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Enums\PanelId;
 use App\Models\Account;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -22,16 +23,29 @@ class AccountPolicy
 
     public function create(User $user): bool
     {
-        return true;
+        return PanelId::APP->isCurrentPanel();
     }
 
     public function update(User $user, Account $account): bool
     {
+        if (PanelId::FAMILY->isCurrentPanel()) {
+            return false;
+        }
+
         return $user->id === $account->user_id;
     }
 
     public function delete(User $user, Account $account): bool
     {
+        if (PanelId::FAMILY->isCurrentPanel()) {
+            return false;
+        }
+
         return $user->id === $account->user_id;
+    }
+
+    public function import(User $user): bool
+    {
+        return PanelId::APP->isCurrentPanel();
     }
 }
