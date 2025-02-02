95ed422b04725dfcf31270dec8d6df90327fcc0c
Chinmay Purav <chinpurav@gmail.com>
2025-02-02 14:15:09 +0530

    abstraction

--- app/Filament/Concerns/UserFilterable.php
+++ app/Filament/Concerns/UserFilterable.php
@@ -3,12 +3,23 @@
 namespace App\Filament\Concerns;
 
 use App\Enums\PanelId;
+use App\Models\User;
+use Filament\Forms\Components\Select;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 
 trait UserFilterable
 {
+    public static function getUserFilterForm(): SelectFilter
+    {
+        return Select::make('users')
+            ->label('Users')
+            ->options(User::pluck('name', 'id'))
+            ->multiple()
+            ->visible(PanelId::FAMILY->isCurrentPanel());
+    }
+
     public static function getUserFilter(): SelectFilter
     {
         return SelectFilter::make('user')
--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -5,10 +5,8 @@ namespace App\Filament\Pages;
 use App\Enums\PanelId;
 use App\Filament\Concerns\UserFilterable;
 use App\Filament\Widgets;
-use App\Models\User;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
 use Filament\Pages\Dashboard as BaseDashboard;
 use Filament\Pages\Dashboard\Concerns\HasFiltersForm;
@@ -23,11 +21,7 @@ class Dashboard extends BaseDashboard
             ->schema([
                 Section::make()
                     ->schema([
-                        Select::make('users')
-                            ->label('Users')
-                            ->options(User::pluck('name', 'id'))
-                            ->multiple()
-                            ->visible(PanelId::FAMILY->isCurrentPanel()),
+                        self::getUserFilterForm(),
                         DatePicker::make('start_date')->default(now()->startOfMonth()),
                         DatePicker::make('end_date')->default(now()->endOfMonth()),
                     ])
