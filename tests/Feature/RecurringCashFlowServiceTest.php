edb74603d40594d87cac98f5bfa928b66a6ec3a8
Chinmay Purav <chinpurav@gmail.com>
2025-02-09 02:27:32 +0530

    expenses test added

--- tests/Feature/RecurringCashFlowServiceTest.php
+++ tests/Feature/RecurringCashFlowServiceTest.php
@@ -45,6 +45,32 @@ function prepareIncomes(User $user): void
         ]);
 }
 
+function prepareExpenses(User $user): void
+{
+    $userAccount = Account::factory()->for($user)->create();
+    RecurringExpense::factory()
+        ->for($user)
+        ->for($userAccount)
+        ->create([
+            'amount' => 1000,
+            'frequency' => Frequency::MONTHLY,
+            'remaining_recurrences' => 12,
+            'next_transaction_at' => today()->addDay(),
+        ]);
+
+    $familyMember = User::factory()->create();
+    $familyMemberAccount = Account::factory()->for($familyMember)->create();
+    RecurringExpense::factory()
+        ->for($familyMember)
+        ->for($familyMemberAccount)
+        ->create([
+            'amount' => 2000,
+            'frequency' => Frequency::MONTHLY,
+            'remaining_recurrences' => 12,
+            'next_transaction_at' => today()->addDay(),
+        ]);
+}
+
 it('returns recurring incomes total only for auth user', function () {
     prepareIncomes($this->user);
     PanelId::APP->setCurrentPanel();
@@ -71,18 +97,9 @@ it('returns recurring incomes total only for all users', function () {
     assertEquals(36000, $total);
 });
 
-it('returns recurring expenses total', function () {
-    $account = Account::factory()->create();
-
-    RecurringExpense::factory()
-        ->for($this->user)
-        ->for($account)
-        ->create([
-            'amount' => 1000,
-            'frequency' => Frequency::MONTHLY,
-            'remaining_recurrences' => 12,
-            'next_transaction_at' => today()->addDay(),
-        ]);
+it('returns recurring expenses total only for auth user', function () {
+    prepareExpenses($this->user);
+    PanelId::APP->setCurrentPanel();
 
     $total = RecurringCashFlowService::processRecurringExpenses(
         $this->user,
@@ -91,4 +108,17 @@ it('returns recurring expenses total', function () {
     );
 
     assertEquals(12000, $total);
+});
+
+it('returns recurring expenses total only for all users', function () {
+    prepareExpenses($this->user);
+    PanelId::FAMILY->setCurrentPanel();
+
+    $total = RecurringCashFlowService::processRecurringExpenses(
+        $this->user,
+        today(),
+        today()->addYear()
+    );
+
+    assertEquals(36000, $total);
 });
