b66bd70cbe6177c90f25ab811b57c964e8bb1b3d
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2025-02-17 02:19:48 +0530

    Feat: handle predated transfers (#52)
    * transfer updated test added
    
    * predated transfer added - logic handle with test
    
    * created observer tested
    
    * deleted observer tested
    
    * test logic placed for updates
    
    * updated logic fix

--- app/Observers/TransferObserver.php
+++ app/Observers/TransferObserver.php
@@ -2,50 +2,125 @@
 
 namespace App\Observers;
 
-use App\Models\Account;
 use App\Models\Transfer;
+use Carbon\Carbon;
 
 readonly class TransferObserver
 {
     public function created(Transfer $transfer): void
     {
-        $transfer->debtor()->decrement('current_balance', $transfer->amount);
-        $transfer->creditor()->increment('current_balance', $transfer->amount);
+        $creditBalance = $this->getCreditBalance(
+            $transfer->creditor->current_balance, $transfer->amount
+        );
+        $debitBalance = $this->getDebitBalance(
+            $transfer->debtor->current_balance, $transfer->amount
+        );
+
+        if ($transactedAt = $this->shouldUpdateCreditorInitialDate($transfer)) {
+            $transfer->creditor->update([
+                'current_balance' => $creditBalance,
+                'initial_date' => $transactedAt,
+            ]);
+        } else {
+            $transfer->creditor()->increment('current_balance', $transfer->amount);
+        }
+
+        if ($transactedAt = $this->shouldUpdateDebtorInitialDate($transfer)) {
+            $transfer->debtor->update([
+                'current_balance' => $debitBalance,
+                'initial_date' => $transactedAt,
+            ]);
+        } else {
+            $transfer->debtor()->decrement('current_balance', $transfer->amount);
+        }
     }
 
     public function updating(Transfer $transfer): void
     {
-        $oldAmount = $transfer->getOriginal('amount');
-        $newAmount = $transfer->getAttribute('amount');
-        $diff = 0;
+        $diff = $this->getUpdatedAmountDiff($transfer);
 
-        if ($transfer->isDirty('amount')) {
-            $diff = $oldAmount - $newAmount;
-            $transfer->debtor()->increment('current_balance', $diff);
-            $transfer->creditor()->decrement('current_balance', $diff);
+        $creditBalance = $this->getCreditBalance(
+            $transfer->creditor->current_balance, $diff
+        );
+        $debitBalance = $this->getDebitBalance(
+            $transfer->debtor->current_balance, $diff
+        );
+
+        if ($transactedAt = $this->shouldUpdateCreditorInitialDate($transfer)) {
+            $transfer->creditor->update([
+                'current_balance' => $creditBalance,
+                'initial_date' => $transactedAt,
+            ]);
+        } else {
+            $transfer->creditor()->increment('current_balance', $diff);
         }
 
-        if ($transfer->isDirty('creditor_id')) {
-            $oldAccount = Account::find($transfer->getOriginal('creditor_id'));
-            $newAccount = Account::find($transfer->getAttribute('creditor_id'));
-
-            $oldAccount->newQuery()->decrement('current_balance', $oldAmount);
-            $newAccount->newQuery()->increment('current_balance', $newAmount + $diff);
+        if ($transactedAt = $this->shouldUpdateDebtorInitialDate($transfer)) {
+            $transfer->debtor->update([
+                'current_balance' => $debitBalance,
+                'initial_date' => $transactedAt,
+            ]);
+        } else {
+            $transfer->debtor()->decrement('current_balance', $diff);
         }
-
-        if ($transfer->isDirty('debtor_id')) {
-            $oldAccount = Account::find($transfer->getOriginal('debtor_id'));
-            $newAccount = Account::find($transfer->getAttribute('debtor_id'));
-
-            $oldAccount->newQuery()->increment('current_balance', $oldAmount);
-            $newAccount->newQuery()->decrement('current_balance', $newAmount + $diff);
-        }
-
     }
 
     public function deleted(Transfer $transfer): void
     {
         $transfer->debtor()->increment('current_balance', $transfer->amount);
         $transfer->creditor()->decrement('current_balance', $transfer->amount);
+    }
+
+    private function shouldUpdateCreditorInitialDate(Transfer $transfer): ?Carbon
+    {
+        $transactedAt = $this->getTransactedAt($transfer);
+
+        if (Carbon::today()->lessThan($transactedAt)) {
+            return null;
+        }
+
+        if ($transfer->creditor->initial_date->lessThan($transactedAt)) {
+            return null;
+        }
+
+        return $transactedAt;
+    }
+
+    private function shouldUpdateDebtorInitialDate(Transfer $transfer): ?Carbon
+    {
+        $transactedAt = $this->getTransactedAt($transfer);
+
+        if (Carbon::today()->lessThan($transactedAt)) {
+            return null;
+        }
+
+        if ($transfer->debtor->initial_date->lessThan($transactedAt)) {
+            return null;
+        }
+
+        return $transactedAt;
+    }
+
+    private function getTransactedAt(Transfer $transfer): Carbon
+    {
+        return $transfer->transacted_at->startOfDay();
+    }
+
+    private function getCreditBalance(float $currentBalance, float $amount): float
+    {
+        return $currentBalance + $amount;
+    }
+
+    private function getDebitBalance(float $currentBalance, float $amount): float
+    {
+        return $currentBalance - $amount;
+    }
+
+    private function getUpdatedAmountDiff(Transfer $transfer): float
+    {
+        $oldAmount = $transfer->getOriginal('amount');
+        $newAmount = $transfer->getAttribute('amount');
+
+        return $newAmount - $oldAmount;
     }
 }
--- tests/Feature/Transfer/TransferOperationSideEffectsTest.php
+++ tests/Feature/Transfer/TransferOperationSideEffectsTest.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Filament\Resources\TransferResource;
 use App\Models\Account;
+use App\Models\Balance;
 use App\Models\Transfer;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-use function Pest\Livewire\livewire;
+use function Pest\Laravel\travelTo;
 
 uses(DatabaseMigrations::class);
 
@@ -19,24 +20,13 @@ it('changes account balances on transfer created', function () {
     $ca = Account::factory()->for($this->user)->create(['current_balance' => 1000]);
     $da = Account::factory()->for($this->user)->create(['current_balance' => 2000]);
 
-    $newData = Transfer::factory()
+    Transfer::factory()
         ->for($this->user)
         ->for($da, 'debtor')
         ->for($ca, 'creditor')
-        ->make([
+        ->create([
             'amount' => 3000,
         ]);
-
-    livewire(TransferResource\Pages\CreateTransfer::class)
-        ->fillForm([
-            'debtor_id' => $newData->debtor->id,
-            'creditor_id' => $newData->creditor_id,
-            'description' => $newData->description,
-            'amount' => $newData->amount,
-            'transacted_at' => $newData->transacted_at,
-        ])
-        ->call('create')
-        ->assertHasNoFormErrors();
 
     $this->assertDatabaseHas(Account::class, [
         'id' => $ca->id,
@@ -47,3 +37,157 @@ it('changes account balances on transfer created', function () {
         'current_balance' => -1000,
     ]);
 });
+
+it('changes account balances on transfer updated', function () {
+    $ca = Account::factory()->for($this->user)->create(['current_balance' => 1000]);
+    $da = Account::factory()->for($this->user)->create(['current_balance' => 2000]);
+
+    $transfer = Transfer::factory()
+        ->for($this->user)
+        ->for($da, 'debtor')
+        ->for($ca, 'creditor')
+        ->today()
+        ->createQuietly([
+            'amount' => 3000,
+        ])->refresh();
+
+    $transfer->update([
+        'amount' => 2000, // amount reduced by 2
+    ]);
+
+    $this->assertDatabaseHas(Account::class, [
+        'id' => $ca->id,
+        'current_balance' => 0,
+    ]);
+    $this->assertDatabaseHas(Account::class, [
+        'id' => $da->id,
+        'current_balance' => 3000,
+    ]);
+});
+
+it('changes account balances on transfer deleted', function () {
+    $ca = Account::factory()->for($this->user)->create(['current_balance' => 1000]);
+    $da = Account::factory()->for($this->user)->create(['current_balance' => 2000]);
+
+    $transfer = Transfer::factory()
+        ->for($this->user)
+        ->for($da, 'debtor')
+        ->for($ca, 'creditor')
+        ->today()
+        ->createQuietly([
+            'amount' => 3000,
+        ])->refresh();
+
+    $transfer->delete();
+
+    $this->assertDatabaseHas(Account::class, [
+        'id' => $ca->id,
+        'current_balance' => -2000,
+    ]);
+    $this->assertDatabaseHas(Account::class, [
+        'id' => $da->id,
+        'current_balance' => 5000,
+    ]);
+});
+
+it('verifies account initial date adjustment when transfer before today added',
+    function (
+        int $beforeCrDay, int $afterCrDay, int $beforeDrDay, int $afterDrDay
+    ) {
+        travelTo(Carbon::create(2025, 01, 20));
+
+        $ca = Account::factory()
+            ->for($this->user)
+            ->create(['current_balance' => 1000, 'initial_date' => Carbon::create(2025, 01, $beforeCrDay)]);
+        $da = Account::factory()
+            ->for($this->user)
+            ->create(['current_balance' => 2000, 'initial_date' => Carbon::create(2025, 01, $beforeDrDay)]);
+
+        Transfer::factory()
+            ->for($this->user)
+            ->for($da, 'debtor')
+            ->for($ca, 'creditor')
+            ->create([
+                'transacted_at' => Carbon::create(2025, 01, 10),
+                'amount' => 3000,
+            ]);
+
+        $this->assertDatabaseHas(Account::class, [
+            'id' => $ca->id,
+            'current_balance' => 4000,
+            'initial_date' => Carbon::create(2025, 01, $afterCrDay),
+        ]);
+        $this->assertDatabaseHas(Balance::class, [
+            'account_id' => $ca->id,
+            'is_initial_record' => true,
+            'recorded_until' => Carbon::create(2025, 01, $afterCrDay),
+        ]);
+        $this->assertDatabaseHas(Account::class, [
+            'id' => $da->id,
+            'current_balance' => -1000,
+            'initial_date' => Carbon::create(2025, 01, $afterDrDay),
+        ]);
+        $this->assertDatabaseHas(Balance::class, [
+            'account_id' => $da->id,
+            'is_initial_record' => true,
+            'recorded_until' => Carbon::create(2025, 01, $afterDrDay),
+        ]);
+    })->with('predated entries');
+
+it('verifies account initial date adjustment when transfer before today updated',
+    function (
+        int $beforeCrDay, int $afterCrDay, int $beforeDrDay, int $afterDrDay
+    ) {
+        travelTo($transactedAt = Carbon::create(2025, 01, 20));
+
+        $ca = Account::factory()
+            ->for($this->user)
+            ->create(['current_balance' => 1000, 'initial_date' => Carbon::create(2025, 01, $beforeCrDay)]);
+        $da = Account::factory()
+            ->for($this->user)
+            ->create(['current_balance' => 2000, 'initial_date' => Carbon::create(2025, 01, $beforeDrDay)]);
+
+        $transfer = Transfer::factory()
+            ->for($this->user)
+            ->for($da, 'debtor')
+            ->for($ca, 'creditor')
+            ->today()
+            ->createQuietly([
+                'transacted_at' => $transactedAt,
+                'amount' => 3000,
+            ])->refresh();
+
+        $transfer->update([
+            'transacted_at' => Carbon::create(2025, 01, 10),
+            'amount' => 2000,
+        ]);
+
+        $this->assertDatabaseHas(Account::class, [
+            'id' => $ca->id,
+            'current_balance' => 0,
+            'initial_date' => Carbon::create(2025, 01, $afterCrDay),
+        ]);
+        $this->assertDatabaseHas(Balance::class, [
+            'account_id' => $ca->id,
+            'is_initial_record' => true,
+            'recorded_until' => Carbon::create(2025, 01, $afterCrDay),
+        ]);
+        $this->assertDatabaseHas(Account::class, [
+            'id' => $da->id,
+            'current_balance' => 3000,
+            'initial_date' => Carbon::create(2025, 01, $afterDrDay),
+        ]);
+        $this->assertDatabaseHas(Balance::class, [
+            'account_id' => $da->id,
+            'is_initial_record' => true,
+            'recorded_until' => Carbon::create(2025, 01, $afterDrDay),
+        ]);
+    })->with('predated entries');
+
+dataset('predated entries', [
+    // before cr day | after cr day | before dr day | after dr day
+    'cr before dr before' => [5, 5, 5, 5],
+    'cr before dr after' => [5, 5, 15, 10],
+    'cr after dr before' => [15, 10, 5, 5],
+    'cr after dr after' => [15, 10, 15, 10],
+]);
