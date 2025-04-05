6d3fa5700240f681bc6f317a537834c585026cfc
Chinmay Purav <chinpurav@gmail.com>
2025-04-05 18:37:08 +0530

    Add ReplicateAction to TransferResource
    Enable replication of transfer records with a default mutation to set the `transacted_at` field to the current timestamp. This enhances usability by allowing quick duplication of existing records.

--- app/Filament/Resources/TransferResource.php
+++ app/Filament/Resources/TransferResource.php
@@ -18,6 +18,7 @@ use Filament\Resources\Resource;
 use Filament\Tables\Actions\BulkActionGroup;
 use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ReplicateAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Enums\FiltersLayout;
 use Filament\Tables\Filters\Filter;
@@ -138,6 +139,12 @@ class TransferResource extends Resource
             ], FiltersLayout::AboveContentCollapsible)
             ->defaultSort('transacted_at')
             ->actions([
+                ReplicateAction::make()
+                    ->mutateRecordDataUsing(function (array $data): array {
+                        $data['transacted_at'] = now();
+
+                        return $data;
+                    }),
                 EditAction::make(),
                 DeleteAction::make(),
             ])
