34ab44cf6813588cf8dddf29c7ea2886020e0e80
Chinmay Purav <chinpurav@gmail.com>
2025-04-13 18:48:09 +0530

    Add people filter tests to Income and Expense pages
    This commit introduces tests verifying the presence of a 'person_id' filter on both the Income and Expense create pages. These tests ensure that the filter correctly displays available people options dynamically.

--- tests/Feature/Expense/ExpenseCreatePageTest.php
+++ tests/Feature/Expense/ExpenseCreatePageTest.php
@@ -3,6 +3,7 @@
 use App\Enums\PanelId;
 use App\Filament\Resources\ExpenseResource\Pages\CreateExpense;
 use App\Models\Account;
+use App\Models\Person;
 use App\Models\User;
 use Filament\Forms\Components\Select;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -26,5 +27,16 @@ it('displays only current user accounts filter', function () {
         ->assertFormFieldExists(
             'account_id',
             checkFieldUsing: fn (Select $field) => $field->getOptions() === [$u1a1->id => $u1a1->name]
+        );
+});
+
+it('displays people filter', function () {
+    $person = Person::factory()
+        ->create();
+
+    livewire(CreateExpense::class)
+        ->assertFormFieldExists(
+            'person_id',
+            checkFieldUsing: fn (Select $field) => $field->getOptions() === [$person->id => $person->name]
         );
 });
