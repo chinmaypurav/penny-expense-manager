e5276d5b53862aa53546b50d30f7ef12bc91eeb0
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2025-04-16 22:43:14 +0530

    Feat: Add Laravel Horizon package and configure monitoring (#79)
    Introduced the Laravel Horizon package for queue monitoring and management. Added configuration to `horizon.php`, registered `HorizonServiceProvider`, and defined access rules via a gate. This setup streamlines queue handling with enhanced visibility into jobs and system performance.

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     */
+    protected function gate(): void
+    {
+        Gate::define('viewHorizon', function (User $user) {
+            return $user->id === 1;
+        });
+    }
+}
