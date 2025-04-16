75d90eeb81111b51192fb9030007fb51895135fe
Chinmay Purav <chinpurav@gmail.com>
2025-04-16 23:29:47 +0530

    Simplify scheduled jobs by removing recurring transactions.
    Removed the scheduling of recurring income, expense, and transfer jobs. The focus is now on periodical balance entry jobs only, likely to streamline operations or transition to a different job handling mechanism.

--- routes/console.php
+++ routes/console.php
@@ -1,48 +1,11 @@
 <?php
 
-use App\Enums\Frequency;
 use App\Enums\RecordType;
 use App\Jobs\CreatePeriodicalBalanceEntryJob;
-use App\Jobs\TransactRecurringExpenseJob;
-use App\Jobs\TransactRecurringIncomeJob;
-use App\Jobs\TransactRecurringTransferJob;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(new CreatePeriodicalBalanceEntryJob(RecordType::MONTHLY, today()))
     ->monthly();
 
 Schedule::job(new CreatePeriodicalBalanceEntryJob(RecordType::YEARLY, today()))
-    ->yearly();
-
-Schedule::job(new TransactRecurringIncomeJob(Frequency::DAILY))
-    ->daily();
-Schedule::job(new TransactRecurringIncomeJob(Frequency::WEEKLY))
-    ->weekly();
-Schedule::job(new TransactRecurringIncomeJob(Frequency::MONTHLY))
-    ->monthly();
-Schedule::job(new TransactRecurringIncomeJob(Frequency::QUARTERLY))
-    ->quarterly();
-Schedule::job(new TransactRecurringIncomeJob(Frequency::YEARLY))
-    ->yearly();
-
-Schedule::job(new TransactRecurringExpenseJob(Frequency::DAILY))
-    ->daily();
-Schedule::job(new TransactRecurringExpenseJob(Frequency::WEEKLY))
-    ->weekly();
-Schedule::job(new TransactRecurringExpenseJob(Frequency::MONTHLY))
-    ->monthly();
-Schedule::job(new TransactRecurringExpenseJob(Frequency::QUARTERLY))
-    ->quarterly();
-Schedule::job(new TransactRecurringExpenseJob(Frequency::YEARLY))
-    ->yearly();
-
-Schedule::job(new TransactRecurringTransferJob(Frequency::DAILY))
-    ->daily();
-Schedule::job(new TransactRecurringTransferJob(Frequency::WEEKLY))
-    ->weekly();
-Schedule::job(new TransactRecurringTransferJob(Frequency::MONTHLY))
-    ->monthly();
-Schedule::job(new TransactRecurringTransferJob(Frequency::QUARTERLY))
-    ->quarterly();
-Schedule::job(new TransactRecurringTransferJob(Frequency::YEARLY))
     ->yearly();
