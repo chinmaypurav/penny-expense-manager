be947044510b39e1836babb64ca7e223129fd858
Chinmay Purav <chinpurav@gmail.com>
2025-05-04 16:26:03 +0530

    Use alias for CarbonImmutable as Carbon in tests
    Replaced `CarbonImmutable` with its alias `Carbon` in `RecordTypeTest` to improve readability and maintain consistency. Updated all instances of `CarbonImmutable` in the test methods and data providers accordingly.

--- tests/Unit/Enums/RecordTypeTest.php
+++ tests/Unit/Enums/RecordTypeTest.php
@@ -1,20 +1,20 @@
 <?php
 
 use App\Enums\RecordType;
-use Carbon\CarbonImmutable;
+use Carbon\CarbonImmutable as Carbon;
 
-it('returns start date', function (RecordType $recordType, CarbonImmutable $inputDate, CarbonImmutable $expectedStartDate) {
+it('returns start date', function (RecordType $recordType, Carbon $inputDate, Carbon $expectedStartDate) {
     $this->assertTrue($recordType->getStartDate($inputDate)->eq($expectedStartDate));
 })->with([
-    [RecordType::INITIAL, CarbonImmutable::parse('2025-02-15'), CarbonImmutable::parse('2025-02-15 00:00:00')],
-    [RecordType::MONTHLY, CarbonImmutable::parse('2025-02-15'), CarbonImmutable::parse('2025-02-01 00:00:00')],
-    [RecordType::YEARLY, CarbonImmutable::parse('2025-02-15'), CarbonImmutable::parse('2025-01-01 00:00:00')],
+    [RecordType::INITIAL, Carbon::parse('2025-02-15'), Carbon::parse('2025-02-15 00:00:00')],
+    [RecordType::MONTHLY, Carbon::parse('2025-02-15'), Carbon::parse('2025-02-01 00:00:00')],
+    [RecordType::YEARLY, Carbon::parse('2025-02-15'), Carbon::parse('2025-01-01 00:00:00')],
 ]);
 
-it('returns end date', function (RecordType $recordType, CarbonImmutable $inputDate, CarbonImmutable $expectedEndDate) {
+it('returns end date', function (RecordType $recordType, Carbon $inputDate, Carbon $expectedEndDate) {
     $this->assertTrue($recordType->getEndDate($inputDate)->is($expectedEndDate));
 })->with([
-    [RecordType::INITIAL, CarbonImmutable::parse('2025-02-15'), CarbonImmutable::parse('2025-02-15 23:59:59')],
-    [RecordType::MONTHLY, CarbonImmutable::parse('2025-02-15'), CarbonImmutable::parse('2025-02-28 23:59:59')],
-    [RecordType::YEARLY, CarbonImmutable::parse('2025-02-15'), CarbonImmutable::parse('2025-12-31 23:59:59')],
+    [RecordType::INITIAL, Carbon::parse('2025-02-15'), Carbon::parse('2025-02-15 23:59:59')],
+    [RecordType::MONTHLY, Carbon::parse('2025-02-15'), Carbon::parse('2025-02-28 23:59:59')],
+    [RecordType::YEARLY, Carbon::parse('2025-02-15'), Carbon::parse('2025-12-31 23:59:59')],
 ]);
