188a7890e92e8a371fdb4cb17ffddcda08aef71c
Chinmay Purav <chinpurav@gmail.com>
2025-05-11 14:08:20 +0530

    wip

--- config/database.php
+++ config/database.php
@@ -92,6 +92,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
+            'dump' => [
+                'add_extra_option' => '--format=c', // and any other pg_dump flags
+            ]
         ],
 
         'sqlsrv' => [
