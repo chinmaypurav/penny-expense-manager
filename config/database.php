cccbbdaec9b61041b06b719d52ca684702fc7f88
Chinmay Purav <chinpurav@gmail.com>
2025-05-11 14:14:29 +0530

    pint

--- config/database.php
+++ config/database.php
@@ -94,7 +94,7 @@ return [
             'sslmode' => 'prefer',
             'dump' => [
                 'add_extra_option' => '--format=c', // and any other pg_dump flags
-            ]
+            ],
         ],
 
         'sqlsrv' => [
