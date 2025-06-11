53c787cc1a02b46b98eb1001b563a08f5f0a2ccb
Chinmay Purav <chinpurav@gmail.com>
2025-06-11 23:02:30 +0530

    AccountObserver.php removed updated side effect

--- app/Observers/AccountObserver.php
+++ app/Observers/AccountObserver.php
@@ -16,43 +16,4 @@ class AccountObserver
             'record_type' => RecordType::INITIAL,
         ]);
     }
-
-    public function updated(Account $account): void
-    {
-        if ($account->isDirty('initial_date') && $account->isDirty('current_balance')) {
-            $diff = $this->getBalanceDifference($account);
-            $account->initialBalance->update([
-                'recorded_until' => $account->initial_date->startOfDay(),
-                'balance' => $account->initialBalance->balance - $diff,
-            ]);
-
-            return;
-        }
-
-        if ($account->isDirty('initial_date')) {
-            $oldBalance = $account->getOriginal('current_balance');
-            $newBalance = $account->getAttribute('current_balance');
-            $diff = $newBalance - $oldBalance;
-
-            $account->initialBalance()->update([
-                'recorded_until' => $account->initial_date->startOfDay(),
-            ]);
-
-            $account->balances()->increment('balance', $diff);
-
-            return;
-        }
-
-        if ($account->isDirty('current_balance')) {
-            $account->initialBalance()->increment('balance', $this->getBalanceDifference($account));
-        }
-    }
-
-    private function getBalanceDifference(Account $account): int
-    {
-        $oldBalance = $account->getOriginal('current_balance');
-        $newBalance = $account->getAttribute('current_balance');
-
-        return $newBalance - $oldBalance;
-    }
 }
