86b6b30c5e75acbe5b7fd3e8e2bfc4ed26bd25cf
Chinmay Purav <chinpurav@gmail.com>
2025-06-11 23:02:54 +0530

    removed related tests

--- tests/Feature/Income/IncomeOperationSideEffectsTest.php
+++ tests/Feature/Income/IncomeOperationSideEffectsTest.php
@@ -2,15 +2,12 @@
 
 use App\Filament\Resources\IncomeResource;
 use App\Models\Account;
-use App\Models\Balance;
 use App\Models\Income;
 use App\Models\Person;
 use App\Models\User;
-use Carbon\Carbon;
 use Filament\Actions\DeleteAction;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-use function Pest\Laravel\travelTo;
 use function Pest\Livewire\livewire;
 
 uses(DatabaseMigrations::class);
@@ -107,77 +104,3 @@ it('subtracts account current_balance when removed', function () {
         'current_balance' => -2000,
     ]);
 });
-
-it('verifies account initial date adjustment when income before today added',
-    function (Carbon $transactedAt, Carbon $expectedAccountInitialDate) {
-        travelTo(Carbon::create(2025, 01, 20));
-
-        $account = Account::factory()->create([
-            'initial_date' => Carbon::create(2025, 01, 10),
-            'current_balance' => 5000,
-        ]);
-        Income::factory()
-            ->for($this->user)
-            ->for($account)
-            ->create([
-                'transacted_at' => $transactedAt,
-                'amount' => 1000,
-            ]);
-
-        $this->assertDatabaseHas(Account::class, [
-            'id' => $account->id,
-            'current_balance' => 6000,
-            'initial_date' => $expectedAccountInitialDate,
-        ]);
-
-        $this->assertDatabaseHas(Balance::class, [
-            'account_id' => $account->id,
-            'is_initial_record' => true,
-            'recorded_until' => $expectedAccountInitialDate,
-        ]);
-    })->with('predated entries');
-
-it('verifies account initial date adjustment when income before today updated',
-    function (Carbon $transactedAt, Carbon $expectedAccountInitialDate) {
-        travelTo(Carbon::create(2025, 01, 20));
-
-        $account = Account::factory()->create([
-            'initial_date' => $initialDate = Carbon::create(2025, 01, 10),
-            'current_balance' => 5000,
-        ]);
-        $income = Income::factory()
-            ->for($this->user)
-            ->for($account)
-            ->createQuietly([
-                'transacted_at' => $initialDate,
-                'amount' => 4000,
-            ])->refresh();
-
-        $income->update([
-            'amount' => 3000, // reduced income by 1000
-            'transacted_at' => $transactedAt,
-        ]);
-
-        $this->assertDatabaseHas(Account::class, [
-            'id' => $account->id,
-            'current_balance' => 4000, // account adjusted by -1000
-            'initial_date' => $expectedAccountInitialDate,
-        ]);
-
-        $this->assertDatabaseHas(Balance::class, [
-            'account_id' => $account->id,
-            'is_initial_record' => true,
-            'recorded_until' => $expectedAccountInitialDate,
-        ]);
-    })->with('predated entries');
-
-dataset('predated entries', [
-    'date before account initial date' => [
-        Carbon::create(2025, 01, 5),
-        Carbon::create(2025, 01, 5),
-    ],
-    'date after account initial date' => [
-        Carbon::create(2025, 01, 15),
-        Carbon::create(2025, 01, 10),
-    ],
-]);
