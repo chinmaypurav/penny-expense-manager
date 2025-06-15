01985c52d3194141a785f7e09c26160a709a9620
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2025-06-15 18:55:01 +0530

    Chore: Configure default password validation rules and prohibit destructive DB commands in production. (#109)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,17 +4,33 @@ namespace App\Providers;
 
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->configureCommands();
+        $this->configurePasswordValidation();
         $this->configureDates();
     }
 
     private function configureDates(): void
     {
         Date::use(CarbonImmutable::class);
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction()
+        );
+    }
+
+    private function configurePasswordValidation(): void
+    {
+        Password::defaults(fn () => $this->app->isProduction() ? Password::min(8)->uncompromised() : null);
     }
 }
