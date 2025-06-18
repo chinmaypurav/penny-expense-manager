c00cd028c5ebcf2ea9ca69d632b3c91967554b98
Chinmay Purav <chinpurav@gmail.com>
2025-06-19 00:01:22 +0530

    route fix

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'app/login');
 
-Route::get('/app/accounts/{account}/transactions', TransactionController::class)->middleware(Authenticate::class);
+Route::get('/app/accounts/{account}/transactions', TransactionController::class)
+    ->name('accounts.transactions')
+    ->middleware(Authenticate::class);
