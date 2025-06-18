3fb353b145d5b0ffcca67b26fb59bc69a090f4df
Chinmay Purav <chinpurav@gmail.com>
2025-06-18 23:55:13 +0530

    basic view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
+use Filament\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'app/login');
+
+Route::get('/app/accounts/{account}/transactions', TransactionController::class)->middleware(Authenticate::class);
